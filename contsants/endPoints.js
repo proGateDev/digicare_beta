34f84e0b4f014807c95250956994da4863f3cc2e
nischal <nischal@progatetechnology.com>
2024-10-01 12:29:14 +0530

    preparing beta ...

--- contsants/endPoints.js
+++ contsants/endPoints.js
@@ -1,5 +1,5 @@
-export const  devURL= "http://192.168.0.172:8000"
-// export const  devURL= "https://digicarebe.onrender.com"
+// export const  devURL= "http://192.168.0.172:8000"
+export const  devURL= "https://digicarebe.onrender.com"
 export const apiEndpoints = {
     getProfile : `${devURL}/admin/profile`,
     userSignUp : `${devURL}/user/auth/signup`,
