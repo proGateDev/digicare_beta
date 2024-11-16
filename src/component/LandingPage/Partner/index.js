1a48a21ad4b154661f8fab86303f8fd35e1933cb
nischal <gupta.nischal014@gmail.com>
2024-11-16 12:53:22 +0530

    apk download from web...

--- src/component/LandingPage/Partner/index.js
+++ src/component/LandingPage/Partner/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 function Index() {
+  const handleDownloadClick = () => {
+    const apkUrl = '/digicare4u.apk'; // Link to the APK in the public folder
+    window.location.href = apkUrl; // Trigger the download
+  };
   return (
     <div className="flex items-center justify-center p-6" >
       <div className="bg-white rounded-lg shadow-lg p-8 max-w-2xl w-full">
-        <h1 className="text-2xl font-bold text-center mb-4 text-blue-600">DigiCare4u: Empowering Care, Anytime, Anywhere</h1>
+        <h1 className="text-2xl font-bold text-center mb-4 text-blue-600">DigiCare4u</h1>
+        <h1 className="text-2xl font-bold text-center mb-4 text-blue-600">Empowering Care, Anytime, Anywhere</h1>
         <div className='flex justify-center items-center '>
-        <img
-              src="/images/logo.jpg"
-              alt="Logo"
-              className="w-34 h-39 cursor-pointer rounded-md" 
-            />
+          <img
+            src="/images/logo.jpg"
+            alt="Logo"
+            className="w-34 h-39 cursor-pointer rounded-md"
+          />
         </div>
-          <div className="flex justify-center mt-5">
+        <div className="flex justify-center mt-5">
           <a
             href="https://www.digicare4u.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             <button
+              onClick={handleDownloadClick}
               style={{
                 background: 'rgba(0, 105, 217, 1) ',
                 color: 'white'
