f247da39afa45918e4df52cfdba2ee4306f335e1
Your Name <youremail@example.com>
2025-01-30 15:41:19 +0530

    admin login

--- src/controllers/admin/signup.js
+++ src/controllers/admin/signup.js
@@ -1,45 +1,45 @@
-import axios from "axios";
-import { devURL } from "../../../contsants/endPoints";
-import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+// import { devURL } from "../../../contsants/endPoints";
+// import { useQuery } from "@tanstack/react-query";
 
 
-export const adduser = async (userData) =>{
-    console.log('token=========')
+// export const adduser = async (userData) =>{
+//     console.log('token=========')
 
-    const token = sessionStorage.getItem('token');
+//     const token = sessionStorage.getItem('token');
 
 
-    const response = await axios.post(`${devURL}/user/auth/signup`,userData, {
-        headers: {
-            Authorization : `Bearer ${token}`,
-            'Content-Type':'application/json'
-        }
-    });
+//     const response = await axios.post(`${devURL}/user/auth/signup`,userData, {
+//         headers: {
+//             Authorization : `Bearer ${token}`,
+//             'Content-Type':'application/json'
+//         }
+//     });
 
-    console.log('response=====', response)
+//     console.log('response=====', response)
     
-    if(response.status !== 201 || !response.data) {
-        console.log('Error', response?.data)
-        throw new Error(`Failed to add User: ${response.statusText}`)
-    }
+//     if(response.status !== 201 || !response.data) {
+//         console.log('Error', response?.data)
+//         throw new Error(`Failed to add User: ${response.statusText}`)
+//     }
     
-    return response.data
+//     return response.data
     
-}
+// }
 
-export const addUser = async (userData) =>{
+// export const addUser = async (userData) =>{
 
-    console.log('')
+//     console.log('')
 
-    const {data, error, isLoading, isFeteching }=  useQuery({
-        queryKey:['userData'],
-        queryFn:()=> adduser(userData)
-    })
+//     const {data, error, isLoading, isFeteching }=  useQuery({
+//         queryKey:['userData'],
+//         queryFn:()=> adduser(userData)
+//     })
 
-    return {
-        isPending : isLoading || isFeteching,
-        error,
-        data
-    }
-}
+//     return {
+//         isPending : isLoading || isFeteching,
+//         error,
+//         data
+//     }
+// }
 
