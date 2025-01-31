52378ab88399179573d3e2a523c1b174d64580b0
Your Name <youremail@example.com>
2025-01-31 14:57:02 +0530

    ...

--- src/pages/user/insights/index.js
+++ src/pages/user/insights/index.js
@@ -1,55 +1,55 @@
-// import React, { useEffect, useState } from "react";
-// import DefaultLayout from "../../../component/Layouts/DefaultLayout";
-// import DetailsCard from "../../../newComponents/user/card/detailsCard";
-// import Teams from "../../../component/teams";
-// // import { useUserMembers } from '/controllers/user/member'
-// import { useUserMembers } from "../../../controllers/user/member";
-// import { useUserProfile } from "../../../controllers/user/profile";
-// import Loader from "../../../component/loader";
-// // import { useAuth } from "../../../context/auth";
+import React, { useEffect, useState } from "react";
+import DefaultLayout from "../../../component/Layouts/DefaultLayout";
+import DetailsCard from "../../../newComponents/user/card/detailsCard";
+import Teams from "../../../component/teams";
+// import { useUserMembers } from '/controllers/user/member'
+import { useUserMembers } from "../../../controllers/user/member";
+import { useUserProfile } from "../../../controllers/user/profile";
+import Loader from "../../../component/loader";
+// import { useAuth } from "../../../context/auth";
 
-// function Index() {
-//   // const { user, logout } = useAuth();
-//   const [loading, setLoading] = useState(true);
-//   const {
-//     isPending: profilePending,
-//     error: profileError,
-//     userProfile,
-//   } = useUserProfile();
-//   const { isPending: membersPending, userMembers } = useUserMembers();
+function Index() {
+  // const { user, logout } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const {
+    isPending: profilePending,
+    error: profileError,
+    userProfile,
+  } = useUserProfile();
+  const { isPending: membersPending, userMembers } = useUserMembers();
 
-//   useEffect(() => {
-//     setLoading(profilePending || membersPending);
-//   }, [profilePending, membersPending]);
+  useEffect(() => {
+    setLoading(profilePending || membersPending);
+  }, [profilePending, membersPending]);
 
-//   // console.log('userProfile===', userProfile)
+  // console.log('userProfile===', userProfile)
 
-//   return (
-//     <>
-//       <DefaultLayout profile={userProfile.user}>
-//         {/* <MemberCard/> */}
-//         {loading ? (
-//           <Loader />
-//         ) : (
-//           <>
-//           {userProfile ? (
-//             <div className="flex flex-col md:flex-row w-full">
-//               <div className="md:container">
-//                 <DetailsCard members={userMembers?.members} />
-//               </div>
-//               <div className="w-full md:w-1/3 p-4">
-//                 <Teams members={userMembers?.members} />
-//               </div>
-//             </div>
+  return (
+    <>
+      <DefaultLayout profile={userProfile.user}>
+        {/* <MemberCard/>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+          {userProfile ? (
+            <div className="flex flex-col md:flex-row w-full">
+              <div className="md:container">
+                <DetailsCard members={userMembers?.members} />
+              </div>
+              <div className="w-full md:w-1/3 p-4">
+                <Teams members={userMembers?.members} />
+              </div>
+            </div>
 
-//           ) : (
-//           <p>Member not Found</p>
-//           )}
-//           </>
-//         )}
-//       </DefaultLayout>
-//     </>
-//   );
-// }
+          ) : (
+          <p>Member not Found</p>
+          )}
+          </>
+        )} */}
+      </DefaultLayout>
+    </>
+  );
+}
 
-// export default Index;
+export default Index;
