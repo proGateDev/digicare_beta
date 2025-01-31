3b6598f82c448f8275f571b5c9ea5ff4ec01c590
Your Name <youremail@example.com>
2025-01-31 16:26:40 +0530

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
+import { Router, useRouter } from "next/router";
 import React from "react";
-// import AdminLogin from "../pages/admin/auth/login"
 
 
 export default function Home() {
+  const router = useRouter()
 
   return (
     <>
-      <div className="bg-white py-4">
-        {/* <AdminLogin/> */}
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+        <h1 className="text-2xl font-bold mb-6">DigiCare4u Login</h1>
+        <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg mb-3 hover:bg-blue-600" onClick={()=>router.push('/Auth/login')}>
+          Login as User
+        </button>
+        <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600" onClick={()=>router.push('/Admin/Auth/login')}>
+          Login as Admin
+        </button>
       </div>
+    </div>
     </>
   );
 }
