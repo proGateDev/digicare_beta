cbfc38cb90fa26437a5512419c23373e690efd0f
Your Name <youremail@example.com>
2025-01-31 16:00:36 +0530

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,5 @@
 import React from "react";
-import AdminLogin from "../pages/admin/auth/login"
-// import Login from "../pages/auth/login"
-// import { useRouter } from 'next/router';
-// import Navbar from "../component/LandingPage/Navbar";
-// import Header from '../component/LandingPage/Header';
-// import Product from '../component/LandingPage/Product'
-// import About from '../component/LandingPage/About'
-// import Review from '../component/LandingPage/Review'
-// import Footer from '../component/LandingPage/Footer'
-// import Features from '../component/LandingPage/Features'
-// import Partner from '../component/LandingPage/Partner'
-// import Hero from '../component/LandingPage/Hero'
-// import { ContactUs } from "../component/LandingPage/ContactUs";
+// import AdminLogin from "../pages/admin/auth/login"
 
 
 export default function Home() {
@@ -19,16 +7,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-white py-4">
-        <AdminLogin/>
-        {/* <Navbar />
-     
-        <Hero />
-        <Header />
-        <Product />
-        <Features />
-        <About />
-        <Partner />
-        <Footer /> */}
+        {/* <AdminLogin/> */}
       </div>
     </>
   );
